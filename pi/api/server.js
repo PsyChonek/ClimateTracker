b29720835f7de2b2324e337ae188e1e87ed184c0
Daniel Vazač <daniel.vazac@email.cz>
2024-12-10 19:46:30 +0100

    date

--- pi/api/server.js
+++ pi/api/server.js
@@ -23,39 +23,50 @@ console.log(`Connecting to ${HOST}:${PORT}`);
 const db = await connect(PORT, HOST, USER, PASSWORD, DATABASE);
 const readings = db.collection("readings");
 
-fastify.get("/allReadings", async (request, reply) => {
-    const { fromDate, toDate } = request.query;
+// POST newReading
+fastify.post("/newReading", async (request, reply) => {
+	const { sensorID, temperature, humidity, timestamp } = request.body;
 
-    const isValidDate = (date) => !isNaN(new Date(date).getTime());
-    const startDate = fromDate && isValidDate(fromDate) ? new Date(fromDate) : new Date(0);
-    const endDate = toDate && isValidDate(toDate) 
-        ? new Date(new Date(toDate).setHours(23, 59, 59, 999)) 
-        : new Date();
+	let timestampNew = timestamp ? new Date(parseInt(timestamp) * 1000).toISOString() : new Date().toISOString();
 
-    console.log("Raw Input Dates:", { fromDate, toDate });
-    console.log("Parsed Query Dates:", { startDate, endDate });
+	var log = {
+		sensorID: sensorID,
+		temperature: temperature,
+		humidity: humidity,
+		timestamp: timestamp,
+		timestampNew: timestampNew,
+	};
+	console.log(log);
 
-    try {
-        // Log all readings to inspect database content
-        const allReadings = await readings.find().toArray();
-        console.log("All Readings:", allReadings);
-
-        // Perform the query
-        const result = await readings.find({
-            timestamp: {
-                $gte: startDate,
-                $lte: endDate,
-            },
-        }).toArray();
-
-        console.log("Query Results:", result);
-        return result;
-    } catch (error) {
-        console.error("Error querying MongoDB:", error);
-        reply.code(500).send({ error: "Failed to fetch readings." });
-    }
+	const newReading = { sensorID, temperature, humidity, timestamp: new Date(timestampNew) };
+	const result = await readings.insertOne(newReading);
+	return result;
 });
 
+fastify.get("/allReadings", async (request, reply) => {
+	const { fromDate, toDate } = request.query;
+
+	const isValidDate = (date) => !isNaN(new Date(date).getTime());
+	const startDate = fromDate && isValidDate(fromDate) ? new Date(fromDate) : new Date(0);
+	const endDate = toDate && isValidDate(toDate) ? new Date(new Date(toDate).setHours(23, 59, 59, 999)) : new Date();
+
+	try {
+		// Perform the query
+		const result = await readings
+			.find({
+				timestamp: {
+					$gte: startDate,
+					$lte: endDate,
+				},
+			})
+			.toArray();
+
+		return result;
+	} catch (error) {
+		console.error("Error querying MongoDB:", error);
+		reply.code(500).send({ error: "Failed to fetch readings." });
+	}
+});
 
 /**
  * Run the server!
