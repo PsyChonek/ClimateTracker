ffb8cca8f5d51764eeeadc8bc44187c46fe7f802
Daniel Vazač <daniel.vazac@email.cz>
2024-12-25 01:20:04 +0100

    Wut

---
+++ services/web-nuxt/components/layout/Bot.vue
@@ -0,0 +1,17 @@
+<template>
+	<div class="flex justify-center items-center p-4 text-white text-center">
+		<div>
+			<p>
+				&copy; Daniel Vazač. Check out my GitHub:
+				<a
+					href="https://github.com/PsyChonek"
+					target="_blank"
+					rel="noopener noreferrer"
+					class="text-white underline"
+				>
+					PsyChonek
+				</a>
+			</p>
+		</div>
+	</div>
+</template>
---
+++ services/web-nuxt/components/layout/Page.vue
@@ -0,0 +1,13 @@
+<template>
+	<div class="flex flex-col min-h-screen">
+		<Top />
+		<Mid />
+		<Bot />
+	</div>
+</template>
+
+<script setup>
+import Top from './Top.vue';
+import Mid from './Mid.vue';
+import Bot from './Bot.vue';
+</script>
